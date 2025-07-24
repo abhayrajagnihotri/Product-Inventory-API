8047190a982a3e78f3441426110106d0258d9afb
abhayrajagnihotri <153421378+abhayrajagnihotri@users.noreply.github.com>
2025-07-24 10:14:59 +0530

    Update models.rs

--- mine/src/models.rs
+++ mine/src/models.rs
@@ -1,7 +1,7 @@
 use serde::{Serialize, Deserialize};
 use uuid::Uuid;
 
-// Represents the full Product structure, including its unique ID.
+.
 #[derive(Serialize, Deserialize, Clone)]
 pub struct Product {
     pub id: Uuid,
@@ -9,8 +9,7 @@ pub struct Product {
     pub price: f64,
 }
 
-// Represents the data structure for creating a new product.
-// Clients will send this, and the server will generate the `id`.
+
 #[derive(Deserialize)]
 pub struct CreateProduct {
     pub name: String,
